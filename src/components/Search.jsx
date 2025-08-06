3a1e544ea835ced23831866ba10d758467df1add
MamoonAhmadKhan <khanmamoon887@gmail.com>
2025-08-06 11:52:08 +0530

    swiggyAPI updated

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
-import { swiggyAPI } from '../api/swiggyAPI';
+import React from 'react';
 
 const Search = () => {
-  const [resturants, setRestaurants] = useState(swiggyAPI);
 
   return (
     <>
       <div className='flex items-center justify-center space-x-2'>
-        <input className='bg-white text-black w-120 p-2 rounded-2xl' type="text" placeholder='Search your favourite Food or Restaurant' />
+        <input 
+        className='bg-white text-black w-120 p-2 rounded-2xl' 
+        type="text" 
+        placeholder='Search your favourite Food or Restaurant' 
+        />
         <button className='bg-white p-2 cursor-pointer rounded-2xl hover:bg-orange-400 hover:text-white'>Search</button>
       </div>
     </>
